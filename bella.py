38a1d7056a9d0a459e08c303c47b63b9d56dc316
Andela <merymckarl@gmail.com>
2019-02-15 18:52:48 +0300

    the year of birth

--- bella.py
+++ bella.py
@@ -1,1 +1,16 @@
-print ("Hello World")
+print ("Bella's age categories\n")
+ 
+print("")
+try:
+    year= int(input("please enter your year of birth: (numbers)\n"))
+    age = 2019 - year
+    age = int(age)
+    if age <18:
+        print('Minor')
+    if age >=18 and age <=36:
+        print("Youth")
+    if age >=36:
+        print("Elder")
+
+except:
+        print("please write in figures or numbers")
